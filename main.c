17a962a4b7d6b9c3b956c65c1600dc30c418a415
c.local <c.local@c.local.com>
2024-08-25 13:18:32 -0400

    initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 3;
+    float b = 4.5;
+    double c = 5.25;
+    float sum;
+
+    sum = a + b + c;
+
+    printf("The sum is %f.", sum);
+    return 0;
+}
